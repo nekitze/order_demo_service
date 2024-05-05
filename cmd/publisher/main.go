e137b19c46907143fb200aca04f8eb189de14b54
kossmali <kossmali@student.21-school.ru>
2024-05-05 17:15:12 +0300

    Publisher script

---
+++ cmd/publisher/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nats-io/nats.go"
+	"log"
+	"os"
+)
+
+func main() {
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to NATS: %v", err)
+	}
+	defer nc.Close()
+
+	fmt.Println("Connected to nats stream. Type 'q' to exit")
+
+	for {
+		var input string
+		fmt.Println("Path to .json file:")
+
+		fmt.Scanln(&input)
+
+		if input == "q" {
+			fmt.Println("Goodbye!")
+			break
+		}
+
+		bytes, err := os.ReadFile(input)
+		if err != nil {
+			log.Printf("Wrong file or path: %s\n", err)
+			continue
+		}
+
+		err = nc.Publish("orders.new", bytes)
+		if err != nil {
+			log.Printf("Publish error: %s\n", err)
+			continue
+		}
+
+		fmt.Println("Published!")
+	}
+}
